bb9ab3e1547ebbd4e0e100c8e09a4138da67cfd6
Gustavo <gustavojsc9@gmail.com>
2025-07-21 20:41:17 -0300

    update: reativando auth middleware

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -2,21 +2,20 @@ const jwt = require('jsonwebtoken');
 
 function authenticateToken(req, res, next) {
 
-  // const token = req.header('Authorization');
+  const token = req.header('Authorization');
 
-  // if (!token) {
-  //   return res.status(401).json({ message: 'Unauthorized' });
-  // }
+  if (!token) {
+    return res.status(401).json({ message: 'Unauthorized' });
+  }
   
-  // // Verifique se o token é válido
-  // jwt.verify(token, process.env.JWT_SECRET_KEY, (err, user) => {
-  //   if (err) {
-  //     return res.status(403).json({ message: 'Token expired or invalid' });
-  //   }
-  //   req.user = user;
-  //   next();
-  // });
-  next();
+  // Verifique se o token é válido
+  jwt.verify(token, process.env.JWT_SECRET_KEY, (err, user) => {
+    if (err) {
+      return res.status(403).json({ message: 'Token expired or invalid' });
+    }
+    req.user = user;
+    next();
+  });
 }
 
 module.exports = authenticateToken;
